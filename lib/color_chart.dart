bb651dd323f4138dccea0a37f01ef518ec120827
James Aprosail <aprosail@outlook.com>
2024-05-16 18:06:02 +0800

    template rm test and app

--- lib/color_chart.dart
+++ lib/color_chart.dart
@@ -1,7 +1,1 @@
-library color_chart;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/mono.dart';
---
+++ lib/src/mono.dart
@@ -0,0 +1,3 @@
+import 'dart:ui';
+
+extension MonoColors on Color {}
